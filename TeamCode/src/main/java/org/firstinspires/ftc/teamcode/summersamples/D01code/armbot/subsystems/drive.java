49141b27d3fdebab1609361e203a55631147e2d4
D02-5irobotics <D02@5irobotics.org>
2025-08-05 19:13:15 -0500

    Update drive.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/summersamples/D01code/armbot/subsystems/drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/summersamples/D01code/armbot/subsystems/drive.java
@@ -3,9 +3,21 @@ package org.firstinspires.ftc.teamcode.summersamples.D01code.armbot.subsystems;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 @TeleOp
-public class drive {
+public class drive extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+        
+    }
+
     public void Move(double y, double x, DcMotor motorfl, DcMotor motorbl, DcMotor motorfr, DcMotor motorbr) {
        motorfl.setPower(y + x);
        motorbl.setPower(y - x);
